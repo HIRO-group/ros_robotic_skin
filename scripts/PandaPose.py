a5e242c11ef1caaab13125da84d7ff4df51d4fd6
Kodur Krishna Chaitanya <chaitanyaradon89@gmail.com>
2020-01-27 16:14:50 -0700

    Velocities working

--- scripts/PandaPose.py
+++ scripts/PandaPose.py
@@ -5,6 +5,7 @@ from std_msgs.msg import String
 from trajectory_msgs.msg import JointTrajectory
 from trajectory_msgs.msg import JointTrajectoryPoint
 from math import pi
+import time
 
 
 class PandaPose:
@@ -13,7 +14,6 @@ class PandaPose:
         self.pub = rospy.Publisher('/panda_arm_controller/command', JointTrajectory, queue_size=1)
         self.pub_int = rospy.Publisher('/joint_mvmt_dof', Int16, queue_size=1)
         self.pub_bool = rospy.Publisher('/calibration_complete', Bool, queue_size=1)
-        self.pub_pose = rospy.Publisher('/panda_pose', String, queue_size=1)
         rospy.init_node('calibration_joint_mvmt_node', anonymous=True)
         self.msg = JointTrajectory()
         self.msg.header.stamp = rospy.Time.now()
@@ -24,12 +24,13 @@ class PandaPose:
         # Set Initial position of the robot
         self.point = JointTrajectoryPoint()
         self.point.positions = [0, 0, 0, 0, 0, 0, 0]
-        self.point.time_from_start.secs = 1
+        self.point.velocities = [0, 0, 0, 0, 0, 0, 0]
+        self.point.time_from_start.secs = 10
         self.msg.points = [self.point]
         # TODO: Look up do we need to have one message to init the robot?
         # If I only send one message then the franka does not move.
-        # TODO: This might not be the best starting postion for the robot to be in.
-        # Think about if we are losing some inforamtion by using a completely vertical postion
+        # TODO: This might not be the best starting position for the robot to be in.
+        # Think about if we are losing some information by using a completely vertical position
         # for the start.
         # Move arm to starting position
         self.pub.publish(self.msg)
@@ -42,23 +43,32 @@ class PandaPose:
             raise Exception("The length of input list should be 7, as panda has 7 arms")
         for index, _ in enumerate(self.point.positions):
             self.point.positions[index] = pose[index]
+
+    def _set_velocity(self, velocity):
+        if len(velocity) != 7:
+            raise Exception("The length of input list should be 7, as panda has 7 arms")
+        for index, _ in enumerate(velocity):
+            self.point.velocities[index] = velocity[index]
+
+    def _set_all_values(self):
         self.msg.points = [self.point]
         if not rospy.is_shutdown():
             # publish message to actuate the dof
             self.pub.publish(self.msg)
-            self.pub_pose.publish(pose_string)
-            rospy.sleep(5)
+            time.sleep(5)
 
     def set_poses(self, poses):
         for each_pose in poses:
-            pose_configuration, pose_string = each_pose[0], each_pose[1]
+            pose_configuration, velocity_configuration, pose_string = each_pose[0], each_pose[1], each_pose[2]
             self._set_pose(pose_configuration, pose_string)
+            # self._set_velocity(velocity_configuration)
+            self._set_all_values()
 
 
 if __name__ == "__main__":
     poses_list = [
-        [[-1, -pi/3, -pi/4, 1, 1, 1, -pi/4], 'Pose_1'],
-        [[-1, -1, -1, -1, -1, -1, -1], 'Pose_2']
+        [[-1, -pi / 3, -pi / 4, 1, 1, 1, -pi / 4], [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01], 'Pose_1'],
+        [[-1, -1, -1, -1, -1, -1, -1], [0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9], 'Pose_2']
     ]
     pp = PandaPose()
     pp.set_poses(poses_list)
