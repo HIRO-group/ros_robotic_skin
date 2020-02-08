82123e84cc71952e944afdeb07b121d22aa96162
Matthew Strong <mast4878@colorado.edu>
2020-02-08 16:41:30 -0700

    deleting commented out code.

---
+++ scripts/activity_matrix.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import rospy 
+import numpy as np
+from std_msgs.msg import Int16
+from std_msgs.msg import Bool
+from sensor_msgs.msg import Imu
+
+import numpy as np
+
+
+class ActivityMatrix():
+    def __init__(self, num_dofs=7, num_skinunits=7):
+        self.num_dofs = num_dofs
+        self.num_skinunits = num_skinunits
+        self.activity_matrix = np.zeros([self.num_skinunits, self.num_dofs])
+        self.current_dof = None
+
+    def spin(self):
+        rospy.init_node('activity_matrix', anonymous=True)
+        rospy.Subscriber('/joint_mvmt_dof', Int16, self.set_dof)
+        rospy.Subscriber('/imu_activated', Int16, self.imu_mvmt)
+        rospy.Subscriber('/calibration_complete', Bool, self.calibration_complete)
+        rospy.spin()
+
+    def set_dof(self, data):
+        # set current dof being actuated
+        self.current_dof = data.data
+
+    def imu_mvmt(self, data):
+        if self.current_dof is not None:
+            # If no DoF has been set then we are just moving the robot to the starting pose
+            print('DOF:', self.current_dof, 'IMU': type(data))
+            print(data.data)
+            self.activity_matrix_matrix[data.data, self.current_dof] = 1
+            print(self.activity_matrix)
+
+
+    def calibration_complete(self, data):
+        if data.data == True:
+            print("****** ACTIVITY MATRIX ******")
+            print(self.activity_matrix)
+            np.savetxt('matrix.txt', self.activity_matrix)
+
+if __name__ == '__main__':
+    activity_matrix = ActivityMatrix()
+    activity_matrix.spin()
--- scripts/imu_listener.py
+++ scripts/imu_listener.py
@@ -38,60 +38,23 @@ class ImuListener():
             z_diff = np.abs(prev_imu_acc[2] - data.linear_acceleration.z)
             total_diff = np.linalg.norm(np.array([x_diff, y_diff, z_diff]))
             if total_diff > self.acc_thresh:
-                # print(x_diff, y_diff, z_diff)
-                # print('*************DIFF***************')
-                # print(data.header.frame_id)
-                # print('NEW DATA')
-                # print('x: {}'.format(data.linear_acceleration.x))
-                # print('y: {}'.format(data.linear_acceleration.y))
-                # print('z: {}'.format(data.linear_acceleration.z))
-                # print('OLD DATA')
-                # print('x: {}'.format(self.prev_imu_matrix[imu_num,0]))
-                # print('y: {}'.format(self.prev_imu_matrix[imu_num,1]))
-                # print('z: {}'.format(self.prev_imu_matrix[imu_num,2]))
-                # print('')
-                # publish the imu message if the acceleration is above a certain threshold.
+                print('*************DIFF***************')
+                print(data.header.frame_id)
+                print('NEW DATA')
+                print('x: {}'.format(data.linear_acceleration.x))
+                print('y: {}'.format(data.linear_acceleration.y))
+                print('z: {}'.format(data.linear_acceleration.z))
+                print('OLD DATA')
+                print('x: {}'.format(self.prev_imu_matrix[imu_num,0]))
+                print('y: {}'.format(self.prev_imu_matrix[imu_num,1]))
+                print('z: {}'.format(self.prev_imu_matrix[imu_num,2]))
+                print('')
+                publish the imu message if the acceleration is above a certain threshold.
                 self.imu_mvmt_pub.publish(Int16(imu_num))
        
             self.prev_imu_matrix[imu_num,0] = data.linear_acceleration.x
             self.prev_imu_matrix[imu_num,1] = data.linear_acceleration.y
             self.prev_imu_matrix[imu_num,2] = data.linear_acceleration.z
 
-# def callback(data):
-#     global prev_imu_link2
-#     global prev_imu_link3
-    
-#     # If we don't have any data on the prev state set the previous measurments
-#     if data.header.frame_id == 'imu_link3' and prev_imu_link3 == None:
-#         prev_imu_link3 = data.linear_acceleration
-#     elif data.header.frame_id == 'imu_link2' and prev_imu_link2 == None:
-#         prev_imu_link2 = data.linear_acceleration
-
-
-#     # Compare the current and prev and see if we have a change that meets the threshold
-#     elif data.header.frame_id == 'imu_link3':
-#         x_diff = abs(prev_imu_link3.x - data.linear_acceleration.x)
-#         y_diff = abs(prev_imu_link3.y - data.linear_acceleration.y)
-#         z_diff = abs(prev_imu_link3.z - data.linear_acceleration.z)
-#         if x_diff > acc_thresh or y_diff > acc_thresh or z_diff > acc_thresh:
-#             # the threshold is large enough to publish an imu activated message
-#             print('*************DIFFF***************')
-#             print(data.header.frame_id)
-#             print('NEW DATA')
-#             print(data.linear_acceleration)
-#             print('OLD DATA')
-#             print(prev_imu_link3)
-#             print('')
-#         prev_imu_link3 = data.linear_acceleration
-    
-#     #prev_imu_link2 = data.linear_acceleration
-    
-# def listener():
-#     rospy.init_node('skin_calibration', anonymous=True)
-#     rospy.Subscriber('imu_data2', Imu, callback)
-#     rospy.Subscriber('imu_data3', Imu, callback)
-#     rospy.spin()
-
 if __name__ == '__main__':
     imu_listener = ImuListener(num_imus=7)
-    # listener()
