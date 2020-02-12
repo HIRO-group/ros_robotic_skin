6c90a6bd6d5656695994f4817797b432aa65cde2
peasant98 <mast4878@colorado.edu>
2020-02-11 18:55:00 -0700

    implemented capture pose

--- scripts/wait_pose.py
+++ scripts/wait_pose.py
@@ -10,20 +10,32 @@ from sensor_msgs.msg import JointState
 
 # a class to capture key poses of the panda 
 
-class CapturePose():
+class WaitPose():
 
     def __init__(self, save_path, poses=11, joints=7):
 
         # publisher for when the user wants the pose to be captured
         rospy.init_node('capture_poses', anonymous=True)
-
         topic_string = "/franka_state_controller/joint_states"
-        self.joint_pos_sub = rospy.Subscriber(topic_string, JointState, self.capture_pose_callback)
-        self.is_in_captured_pose = False
+        
+        self.is_in_captured_pose_sub = rospy.Publisher("is_in_captured_pose", Bool)
         self.poses = poses
-        self.captured_positions = np.zeros((poses, joints))
-        self.save_path = os.path.join(save_path, 'data', 'positions.txt')
+        self.pose_num = 0
         rospy.spin()
+
+    def capture_poses(self):
+        for i in range(self.poses):
+            self.pose_num = i
+            self.is_in_captured_pose_sub.publish(Bool(False))
+            print("Please move the robot to the desired pose.")
+            raw_input("Press enter to capture the pose: ")
+            self.is_in_captured_pose_sub.publish(Bool(True))
+            print("==========================")
+            print("Please wait 2 seconds in this pose...")
+            # sleep for 5 seconds
+            rospy.sleep(2)
+        np.savetxt(self.save_path, self.captured_positions)
+
 
     def capture_pose_callback(self, data):
         if self.is_in_captured_pose:
