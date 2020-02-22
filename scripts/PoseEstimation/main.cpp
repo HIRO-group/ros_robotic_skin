8ea3dc09ca1ba753c6a3a4aa0b646156a006bd6d
Kodur Krishna Chaitanya <chaitanyaradon89@gmail.com>
2020-02-21 23:01:54 -0700

    Setup the IDE and starting the development

--- scripts/PoseEstimation/main.cpp
+++ scripts/PoseEstimation/main.cpp
@@ -1,6 +1,45 @@
 #include <cmath>
 #include <imu_filter_madgwick/imu_filter.h>
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "ros/ros.h"
+#include "trajectory_msgs/JointTrajectory.h"
+#include "trajectory_msgs/JointTrajectoryPoint.h"
+#include "sensor_msgs/Imu.h"
+#define FILTER_ITERATIONS 10000
+
+
+template <WorldFrame::WorldFrame FRAME>
+void filterStationary(
+        float Ax, float Ay, float Az,
+        float Mx, float My, float Mz,
+        double& q0, double& q1, double& q2, double& q3) {
+    float dt = 0.1;
+    float Gx = 0.0, Gy = 0.0, Gz = 0.0; // Stationary state => Gyro = (0,0,0)
+
+    ImuFilter filter;
+    filter.setDriftBiasGain(0.0);
+    filter.setAlgorithmGain(0.1);
+
+    // initialize with some orientation
+    filter.setOrientation(q0,q1,q2,q3);
+    filter.setWorldFrame(FRAME);
+
+    for (int i = 0; i < FILTER_ITERATIONS; i++) {
+        filter.madgwickAHRSupdate(Gx, Gy, Gz, Ax, Ay, Az, Mx, My, Mz, dt);
+    }
+
+    filter.getOrientation(q0,q1,q2,q3);
+}
+
+void msgCallback(const sensor_msgs::Imu::ConstPtr& msg){
+    std::cout << "IMU2 Data: X: " << msg->linear_acceleration.x << " Y: " << msg->linear_acceleration.y << " Z: "
+        << msg->linear_acceleration.z;
+
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc,argv,"IMU_Pose_Estimator");
+    ros::NodeHandle nh;
+    ros::Subscriber cam_sub = nh.subscribe("/imu_data2",100,msgCallback);
+    ros::spin();
     return 0;
 }
