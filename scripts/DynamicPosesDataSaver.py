312bf1022f3bfa941f73ddfdc80e3502c9b7aa5e
Kandai <kandai.wata@gmail.com>
2020-02-22 22:07:18 -0700

    [WIP] fixed orientation

--- scripts/DynamicPosesDataSaver.py
+++ scripts/DynamicPosesDataSaver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os, sys
-from collections import defaultdict
+from collections import OrderedDict
 import copy
 import datetime
 import math
@@ -16,6 +16,23 @@ from SawyerController import SawyerController
 from PandaController import PandaController
 
 RAD2DEG = 180.0/np.pi
+OSCILLATION_TIME = 3.0
+
+n2s = lambda x, precision=2:  np.array2string(x, precision=precision, separator=',', suppress_small=True)
+
+def hampel_filter_forloop(input_series, window_size, n_sigmas=3):
+    n = len(input_series)
+    new_series = input_series.copy()
+    k = 1.4826 # scale factor for Gaussian distribution
+    indices = []
+    # possibly use np.nanmedian 
+    for i in range((window_size),(n - window_size)):
+        x0 = np.median(input_series[(i - window_size):(i + window_size)])
+        S0 = k * np.median(np.abs(input_series[(i - window_size):(i + window_size)] - x0))
+        if (np.abs(input_series[i] - x0) > n_sigmas * S0):
+            new_series[i] = x0
+            indices.append(i)
+    return new_series, indices
 
 class DynamicPoseData():
     """
@@ -28,7 +45,6 @@ class DynamicPoseData():
     def __init__(self, pose_names, joint_names, imu_names, filepath):
         """
         Initialize StaticPoseData class. 
-
         Arguments
         ----------
         pose_names: list[str]
@@ -44,13 +60,13 @@ class DynamicPoseData():
         self.joint_names = joint_names
         self.imu_names = imu_names
         self.filepath = filepath
-        self.data = defaultdict(list)
+        self.data = OrderedDict()
 
         # Create nested dictionary to store data
         for pose_name in pose_names:
-            self.data[pose_name] = defaultdict(list)
+            self.data[pose_name] = OrderedDict()
             for joint_name in joint_names:
-                self.data[pose_name][joint_name] = defaultdict(list)
+                self.data[pose_name][joint_name] = OrderedDict()
                 for imu_name in imu_names:
                     self.data[pose_name][joint_name][imu_name] = np.empty((0, 4), float)
 
@@ -58,7 +74,6 @@ class DynamicPoseData():
         """
         Append data to a dictionary whose keys are 
         [pose_name][joint_name][imu_name]
-
         Arguments
         ----------
         pose_name: str
@@ -77,10 +92,8 @@ class DynamicPoseData():
     def _save(self, data, suffix=None):
         """
         Save data as a pickle file
-
         data: 
             Nested Dictionary
-
         suffix: str
             suffix to add to the filename
         """
@@ -91,23 +104,26 @@ class DynamicPoseData():
         with open(filepath, 'wb') as f:
             pickle.dump(data, f)
 
-    def save(self):
+    def save(self, verbose=False):
         """
         TODO: Stop saving the original collected data since we do not need them
         Save both the original collected data and the maxed data
         """
-        # save original
-        self._save(self.data)
-
         # Create nested dictionary to store data
         data = copy.deepcopy(self.data)
         for pose_name in self.pose_names:
             for joint_name in self.joint_names:
                 for imu_name in self.imu_names:
-                    norm = np.linalg.norm(self.data[pose_name][joint_name][imu_name][:,:3], axis=1)
-                    idx = np.argmax(norm, axis=1)
+                    accel_z = self.data[pose_name][joint_name][imu_name][:, 2]
+                    accel_z, outliers_index = hampel_filter_forloop(accel_z, 10, 3)
+                    idx = np.argmax(np.abs(accel_z))
+                    d = self.data[pose_name][joint_name][imu_name][idx, :]
                     data[pose_name][joint_name][imu_name] = \
                         self.data[pose_name][joint_name][imu_name][idx, :]
+
+                    if verbose:
+                        d = self.data[pose_name][joint_name][imu_name][idx, :]
+                        rospy.loginfo('[%s, %s, %s] (%.3f, %.3f, %.3f)'%(pose_name, joint_name, imu_name, d[0], d[1], d[2]))
 
         # save max acceleration data
         self._save(data, "_max")
@@ -119,7 +135,6 @@ class DynamicPoseDataSaver():
     def __init__(self, controller, poses_list, filepath='data/dynamic_data'):
         """
         Initializes DynamicPoseDataSaver class.
-
         Arguments
         -----------
         controller: 
@@ -165,6 +180,9 @@ class DynamicPoseDataSaver():
             accel = data.linear_acceleration
             joint_angle = self.controller._limb.joint_angle(self.curr_joint_name)
 
+            # if self.curr_joint_name == 'right_j0' and data.header.frame_id == 'imu_link0':
+            #     rospy.loginfo(n2s(np.array([accel.x, accel.y, accel.z])))
+
             self.data_storage.append(
                 self.curr_pose_name,            # for each defined initial pose
                 self.curr_joint_name,           # for each excited joint
@@ -195,12 +213,14 @@ class DynamicPoseDataSaver():
                 # Prepare for publishing a trajectory
                 velocities = np.zeros(len(self.joint_names))
                 accelerations = np.zeros(len(self.joint_names))
-                poss = positions
+                poss = copy.deepcopy(positions)
 
                 # stopping time
-                d1 = datetime.datetime.now() + datetime.timedelta(seconds=5)
                 self.ready = True
+                now = rospy.get_rostime()
                 while True:
+                    t = (rospy.get_rostime() - now).to_sec()
+
                     # Oscillated Pos, Vel, Acc
                     position = A/(2*pi*freq)*(1-math.cos(2 * pi * freq * t))
                     velocity = A*math.sin(2 * pi * freq * t)
@@ -209,43 +229,47 @@ class DynamicPoseDataSaver():
                     poss[i] = positions[i] + position
                     velocities[i] = velocity
                     accelerations[i] = acceleration
-                    
-                    self.controller.publish_trajectory(positions, velocities, accelerations, None)
 
-                    t += dt
-                    if d1 < datetime.datetime.now():
+                    self.controller.publish_trajectory(poss, velocities, accelerations, None)
+
+                    if t > OSCILLATION_TIME:
                         break
                 self.ready = False
+                rospy.sleep(1)
 
-    def save(self):
+    def save(self, verbose=False):
         """
         Save data to a pickle file.
         """
-        self.data_storage.save()
+        self.data_storage.save(verbose)
 
 if __name__ == "__main__":
     # Poses Configuration
     poses_list = [
-        [[3.47, -2.37, 1.38, 0.22, 3.13, 1.54, 1.16], [], 'Pose_1'],
-        [[-1.10, -2.08, 5.68, 1.41, 4.13, 0.24, 2.70], [], 'Pose_2'],
-        [[-0.75, -1.60, 1.56, 4.43, 1.54, 4.59, 6.61], [], 'Pose_3'],
-        [[-0.61, -0.54, 3.76, 3.91, 5.05, 0.92, 6.88], [], 'Pose_4'],
-        [[-1.39, -0.87, 4.01, 3.75, 5.56, 2.98, 4.88], [], 'Pose_5'],
-        [[1.51, -2.47, 3.20, 1.29, 0.24, 4.91, 8.21], [], 'Pose_6'],
-        [[0.25, -0.18, 5.13, 5.43, 2.78, 3.86, 6.72], [], 'Pose_7'],
-        [[0.76, -1.96, 2.24, 1.54, 4.19, 5.22, 7.46], [], 'Pose_8'],
-        [[0.03, -1.09, 2.63, 0.33, 3.87, 0.88, 2.92], [], 'Pose_9'],
-        [[0.72, -1.00, 6.09, 2.61, 1.10, 4.13, 3.06], [], 'Pose_10'],
-        [[1.69, -2.72, 0.14, 1.08, 2.14, 0.08, 9.13], [], 'Pose_11'],
-        [[0.81, -1.89, 3.26, 1.42, 5.64, 0.14, 8.34], [], 'Pose_12'],
-        [[-0.90, -3.10, 3.24, 0.16, 4.81, 4.94, 4.35], [], 'Pose_13'],
-        [[1.36, -1.89, 2.73, 1.20, 3.08, 3.29, 3.88], [], 'Pose_14'],
+        [[0, 0, 0, 0, 0, 0, 0], [], 'Pose_1'],
+        [[3.47, -2.37, 1.38, 0.22, 3.13, 1.54, 1.16], [], 'Pose_2'],
+        [[-1.10, -2.08, 5.68, 1.41, 4.13, 0.24, 2.70], [], 'Pose_3'],
+        [[-0.75, -1.60, 1.56, 4.43, 1.54, 4.59, 6.61], [], 'Pose_4'],
+        [[-0.61, -0.54, 3.76, 3.91, 5.05, 0.92, 6.88], [], 'Pose_5'],
+        [[-1.39, -0.87, 4.01, 3.75, 5.56, 2.98, 4.88], [], 'Pose_6'],
+        [[1.51, -2.47, 3.20, 1.29, 0.24, 4.91, 8.21], [], 'Pose_7'],
+        [[0.25, -0.18, 5.13, 5.43, 2.78, 3.86, 6.72], [], 'Pose_8'],
+        [[0.76, -1.96, 2.24, 1.54, 4.19, 5.22, 7.46], [], 'Pose_9'],
+        [[0.03, -1.09, 2.63, 0.33, 3.87, 0.88, 2.92], [], 'Pose_10'],
+        [[0.72, -1.00, 6.09, 2.61, 1.10, 4.13, 3.06], [], 'Pose_11'],
+        [[1.69, -2.72, 0.14, 1.08, 2.14, 0.08, 9.13], [], 'Pose_12'],
+        [[0.81, -1.89, 3.26, 1.42, 5.64, 0.14, 8.34], [], 'Pose_13'],
+        [[-0.90, -3.10, 3.24, 0.16, 4.81, 4.94, 4.35], [], 'Pose_14'],
+        [[1.36, -1.89, 2.73, 1.20, 3.08, 3.29, 3.88], [], 'Pose_15'],
         [[-0.36, -2.19, 3.91, 0.04, 2.15, 3.19, 5.18], [], 'Pose_16'],
         [[5.25, -0.55, 0.98, 4.15, 5.65, 3.65, 9.27], [], 'Pose_17'],
         [[2.52, -2.54, 2.07, 0.55, 3.26, 2.31, 4.72], [], 'Pose_18'],
         [[4.63, -0.70, 3.14, 3.41, 3.55, 0.69, 6.10], [], 'Pose_19'],
         [[5.41, -0.90, 5.86, 0.41, 1.69, 1.23, 4.34], [], 'Pose_20']
     ]    
+    """
+    ]
+    """
 
     # [Pose, Joint, IMU, x, y, z]* number os samples according to hertz
     arg = sys.argv[1]
@@ -259,4 +283,4 @@ if __name__ == "__main__":
     filepath = 'data/dynamic_data'
     dd = DynamicPoseDataSaver(controller, poses_list, filepath)
     dd.move_like_sine_dynamic()
-    dd.save()
+    dd.save(verbose=True)
