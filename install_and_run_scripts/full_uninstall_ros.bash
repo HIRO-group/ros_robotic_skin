db6aa6885d6a3a8c2b78c9359e6500cce3e297af
Krishna <chaitanyaradon89@gmail.com>
2020-03-14 12:55:34 -0600

    Adding just install.sh

--- install_and_run_scripts/full_uninstall_ros.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt remove --purge ros-*
-sudo apt purge --auto-remove ros-melodic-desktop-full
-sudo rm -rf /etc/ros
-echo "I guess ROS is now fully removed, check if any files exist in /opt/ros and then dont forget to restart"
--- install_and_run_scripts/get_me_catkin_environment.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CATKIN_WORKSPACE=$(pwd)
-PROJECT_DEVEL_SETUP=$CATKIN_WORKSPACE/devel/setup.bash
-source ~/.bashrc
-source /opt/ros/melodic/setup.bash
-source "$PROJECT_DEVEL_SETUP"
