17844252c64621e8d57eb174554f682f111a3ea9
Matthew Strong <mast4878@colorado.edu>
2020-03-14 19:32:54 -0600

    mistake fixed

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ then
   cmake -DCMAKE_BUILD_TYPE=Release ..
   cmake --build .
   cd ../../..
-  catkin_make -j4 -DCMAKE_BUILD_TYPE=Release -DFranka_DIR:PATH=/path/to/libfranka/build
+  catkin_make -j4 -DCMAKE_BUILD_TYPE=Release -DFranka_DIR:PATH=$(pwd)/src/libfranka/build
 
 else
   sudo apt-get install ros-melodic-libfranka
