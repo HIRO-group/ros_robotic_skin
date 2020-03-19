5fb51ceeb2db4dc34a0c6bb0dce26e2f567d1a9d
Krishna <chaitanyaradon89@gmail.com>
2020-03-19 04:22:03 -0600

    Remove some text

--- install_rpi.sh
+++ install_rpi.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Boss: Is there need of such script?
-# Me: Is there need to install franka ROS, libfranka, sawyer and 1000 other things?
-# Boss: Would it hurt?
-# Me: No but it takes more than many hours to do catkin_make when all you want to do send IMU messages
-# Boss: _______________
 source /opt/ros/melodic/setup.bash
 # Below line will be activated when all requirements are sorted out. Right now they are not
 #pip3 install --upgrade git+https://github.com/HIRO-group/robotic_skin.git
