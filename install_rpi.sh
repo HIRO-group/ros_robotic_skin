537e1105a05b8ed8db358ff02496a5619a4114a9
Krishna <chaitanyaradon89@gmail.com>
2020-03-19 01:50:53 -0600

    Add Raspberry Pi install script

---
+++ install_rpi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Boss: Is there need of such script?
+# Me: Is there need to install franka ROS, libfranka, sawyer and 1000 other things?
+# Boss: Would it hurt?
+# Me: No but it takes more than many hours to do catkin_make when all you want to do send IMU messages
+# Boss: _______________
+source /opt/ros/melodic/setup.bash
+# Below line will be activated when all requirements are sorted out. Right now they are not
+#pip3 install --upgrade git+https://github.com/HIRO-group/robotic_skin.git
+# checks to see if a valid catkin workspace has been created
+TWO_DOTS=$(cd ../../ && pwd)
+THREE_DOTS=$(cd ../../.. && pwd)
+if [[ -d ../../src && $TWO_DOTS != $THREE_DOTS ]]; then
+    echo "Dir exists"
+else
+    echo "Not a valid catkin workspace!"
+    echo "Terminating installation..."
+    # exit the whole thing
+    exit 1
+fi
+cd ../.. #  Move to catkin_workspace
+catkin_make
+
+
