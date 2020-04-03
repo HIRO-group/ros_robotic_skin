6e8e4c13614086973d4fc8d888f30579993e9bd1
Ander <ander@pop-os.localdomain>
2020-04-03 17:15:38 -0600

    Control points broadcaster and custom launch file

---
+++ src/control_points_broadcaster.cpp
@@ -0,0 +1,91 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <visualization_msgs/Marker.h>
+
+void publish_control_point(tf::TransformBroadcaster& br,
+                           tf::Transform& transform,
+                           ros::Publisher& marker_pub,
+                           int link_number,
+                           float z,
+                           float sphere_radius )
+{
+    int i;
+    switch (link_number)
+    {
+        case 3: i = 1; break;
+        case 5: i = 2; break;
+        default: break;
+    }
+    std::string name;
+    std::string parent;
+    name = "control_point" + std::to_string(i);
+    parent = "panda_link" + std::to_string(link_number);
+    transform.setOrigin( tf::Vector3(0.0, 0.0, z) );
+    transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), parent, name));
+}
+
+void visualize_sphere(ros::Publisher& marker_pub, visualization_msgs::Marker& marker, int control_point_number)
+{
+  // Marker setup
+    marker.header.frame_id = "/control_point" + std::to_string(control_point_number);
+    marker.header.stamp = ros::Time::now();
+    marker.ns = "basic_shapes";
+    marker.id = control_point_number;
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.action = visualization_msgs::Marker::ADD;
+    // Position and orientation
+    marker.pose.position.x = 0;
+    marker.pose.position.y = 0;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    float radius = 0.15;
+
+    marker.scale.x = radius;
+    marker.scale.y = radius;
+    marker.scale.z = radius;
+
+    marker.color.r = 1.0f;
+    marker.color.g = 0.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 0.5;
+
+    marker.lifetime = ros::Duration(1);
+
+    marker_pub.publish(marker);
+}
+
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "control_point_broadcaster");
+    ros::NodeHandle node;
+
+    tf::TransformBroadcaster br;
+    tf::Transform transform;
+
+    ros::Rate rate(50.0);
+
+    ros::Publisher marker_pub = node.advertise<visualization_msgs::Marker>("visualization_markers", 1);
+    visualization_msgs::Marker marker;
+
+
+    while (node.ok())
+    {
+
+        publish_control_point(br, transform, marker_pub, 3, -0.15, 0.2);
+        publish_control_point(br, transform, marker_pub, 5, -0.15, 0.2);
+
+        for (int i = 1; i < 3; i++)
+        {
+            visualize_sphere(marker_pub, marker, i);
+        }
+
+
+        rate.sleep();
+    }
+    return 0;
+};
