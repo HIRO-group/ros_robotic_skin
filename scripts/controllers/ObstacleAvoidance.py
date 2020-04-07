c3e713ce304e448ced303f0b475b6b40501be3f9
Ander <ander@pop-os.localdomain>
2020-04-07 16:42:41 -0600

    Set up for end effector controller (Garett)

--- scripts/controllers/ObstacleAvoidance.py
+++ scripts/controllers/ObstacleAvoidance.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from PandaController import PandaController
+from CartesianController import CartesianController
 import numpy as np
 import rospy
 import tf
 
 
-class ObstacleAvoidance(object):
+class ObstacleAvoidance(CartesianController):
     def __init__(self):
 
         self.panda_controller = PandaController()
@@ -44,17 +45,31 @@ class ObstacleAvoidance(object):
                     self.control_points.append(point)
 
     def get_obstacle_points(self):
-        self.obstacle_points = [np.array([0., 0.,  0.333]),
-                                np.array([-0.19582668, -0.24056836,  0.2727109]),
-                                np.array([-0.09791334, -0.12028418,  0.30285545]),
-                                np.array([-0.13986038, -0.27334841,  0.22172569]),
-                                np.array([-0.16784353, -0.25695838,  0.24721829]),
-                                np.array([0.21203928, -0.26132252,  0.04770196]),
-                                np.array([0.03608945, -0.26733546,  0.13471382]),
-                                np.array([0.27500713, -0.24394749,  0.1066694]),
-                                np.array([0.2435232, -0.252635,  0.07718568]),
-                                np.array([0.20461844, -0.25810337,  0.18600441]),
-                                np.array([0.23981278, -0.25102543,  0.1463369])]
+        self.obstacle_points = [np.array([0., 0.,  0.333])]
+
+    def go_to_point(self, position_desired):
+        """
+        Move to a point an stop when arrived
+
+        Parameters
+        ----------
+        position_desired : numpy.ndarray
+            3x1 end effector desired position vector
+            Only position, not orientation
+        """
+        position_desired = np.array(position_desired)
+        self.error = position_desired - self.position
+        while np.linalg.norm(self.error) > self.error_threshold:
+            self.position = self.__get_current_end_effector_position()
+            xd_dot = self.__compute_command_velocity(position_desired)
+            print(xd_dot)
+            # Add flacco algorithm for end effector
+            # xc_dot = end_effector_algorithm(xd_dot)
+            # self.q_dot = self.__compute_command_q_dot(xc_dot)
+            self.panda_controller.send_velocities(self.q_dot)
+            self.rate.sleep()
+        self.stop()
+        return 0
 
 
 if __name__ == "__main__":
