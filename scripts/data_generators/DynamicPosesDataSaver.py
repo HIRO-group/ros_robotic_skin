465c97fed4b542eec35ae6e244ea9937d95c6e40
Matthew Strong <mast4878@colorado.edu>
2020-04-17 09:48:14 -0600

    moved to utils

--- scripts/data_generators/DynamicPosesDataSaver.py
+++ scripts/data_generators/DynamicPosesDataSaver.py
@@ -18,97 +18,8 @@ from scripts import utils  # noqa: E402
 from scripts.controllers.PandaController import PandaController  # noqa: E402
 from scripts.controllers.SawyerController import SawyerController  # noqa: E402
 
-
 RAD2DEG = 180.0 / np.pi
 OSCILLATION_TIME = 3.0
-
-
-def hampel_filter_forloop(input_series, window_size, n_sigmas=3):
-    """
-    Implementation of Hampel Filter for outlier detection.
-
-    Arguments
-    ----------
-    `input_series`: `np.array`
-        The input data to use for outlier detection.
-
-    `window_size`: `int`
-        The sliding window size to use for the filter on
-        `input_series`.
-
-    `n_sigmas`: `int`
-        The number of standard deviations to determine
-        what data points are outliers.
-    """
-    n = len(input_series)
-    new_series = input_series.copy()
-    k = 1.4826  # scale factor for Gaussian distribution
-    indices = []
-    # possibly use np.nanmedian
-    for i in range((window_size), (n - window_size)):
-        x0 = np.median(input_series[(i - window_size):(i + window_size)])
-        S0 = k * np.median(np.abs(input_series[(i - window_size):(i + window_size)] - x0))
-        if (np.abs(input_series[i] - x0) > n_sigmas * S0):
-            new_series[i] = x0
-            indices.append(i)
-    return new_series, indices
-
-
-def moving_avg_low_pass_filter(data, window_size):
-    """
-    Implementation of a standard moving average low pass filter.
-
-    Arguments
-    ---------
-    `data`: `np.array`
-        data to be filtered.
-
-    `window_size`: `int`
-        Window size from which the average is calculated
-        for each point
-
-    """
-    n = len(data)
-    new_data = data.copy()
-
-    for i in range(0, (n - window_size)):
-        avg = np.mean(data[i: i + window_size])
-        new_data[i] = avg
-    return new_data
-
-
-def low_pass_filter(data, samp_freq, cutoff_freq=15.):
-    """
-    Implementation of the standard pass filter,
-    also known as a exponential moving average filter.
-
-    Arguments
-    ---------
-    `data`:
-        data to be filtered.
-    `samp_freq`:
-        sampling frequency of the data
-    `cutoff_freq`:
-        cutoff frequency; that is, data that is > = `cutoff_freq` will
-        be attentuated.
-    """
-    # need to cut cutoff_freq in half because we apply two filters.
-    half_cutoff_freq = cutoff_freq * 0.5
-    n = len(data)
-    # smoother data when alpha is lower
-    tau = 1 / (2 * np.pi * half_cutoff_freq)
-    dt = 1 / samp_freq
-    alpha = dt / (dt + tau)
-    new_data = data.copy()
-
-    for i in range(1, n):
-        new_data[i] = ((1 - alpha) * new_data[i-1]) + (alpha * data[i])
-    reversed_data = new_data[::-1]
-
-    for i in range(1, n):
-        reversed_data[i] = ((1 - alpha) * reversed_data[i-1]) + (alpha * reversed_data[i])
-
-    return reversed_data[::-1]
 
 
 class DynamicPoseData():
@@ -202,11 +113,11 @@ class DynamicPoseData():
                     joint_accs = imu_data[:, 6]
 
                     # filter acceleration norms
-                    filtered_norms = low_pass_filter(norms, 100.)
-                    # filtered_vels = low_pass_filter(ang_vels, 100.)
+                    filtered_norms = utils.low_pass_filter(norms, 100.)
+                    # filtered_vels = utils.low_pass_filter(ang_vels, 100.)
 
                     # filter joint accelerations
-                    filtered_joint_accs = low_pass_filter(joint_accs, 100.)
+                    filtered_joint_accs = utils.low_pass_filter(joint_accs, 100.)
                     # array of imu data - both filtered and raw
                     imu_filtered_arr = []
                     imu_raw_arr = []
