f95ac1e453d86b00ea8fea7e9576886035cbe7ce
Ander <ander@pop-os.localdomain>
2020-04-23 13:52:33 -0600

    Clean ObstacleAvoidance

--- scripts/controllers/ObstacleAvoidance.py
+++ scripts/controllers/ObstacleAvoidance.py
@@ -4,10 +4,11 @@ from CartesianPositionController import CartesianPositionController
 import numpy as np
 import rospy
 import tf
-# import matplotlib.pyplot as plt
 
-MAX_RATE = 1.0
 NUMBER_OF_CONTROL_POINTS = 1
+
+# Parameters of the flacco paper
+MAX_RATE = 1.0  # Obstacle velocity estimation equation
 ALPHA = 6.0
 C = 5.0
 RHO = 0.4
@@ -16,18 +17,18 @@ Q_DOT_MIN = np.array([-2.1750, -2.1750, -2.1750, -2.1750, -2.6100, -2.6100, -2.6
 Q_DOT_MAX = np.array([+2.1750, +2.1750, +2.1750, +2.1750, +2.6100, +2.6100, +2.6100])
 
 
-class ObstacleAvoidance(CartesianPositionController):
+class ObstacleAvoidanceController(CartesianPositionController):
     def __init__(self):
-
-        super(ObstacleAvoidance, self).__init__()
-        self.position = np.zeros(3)
+        super(ObstacleAvoidanceController, self).__init__()
         self.control_points = []
         self.obstacle_points = [np.array([500, 500, 500])]  # Very far away
         self.Vi = np.zeros(3)
-        # self.Vi_last = np.zeros(3)
 
     def get_control_points(self):
-        # t = time.time()
+        """
+        Updates the location of the end_effector in self.position.
+        Updates the location of the control points in self.control_points.
+        """
         self.control_points = []
         while True:
             try:
@@ -42,19 +43,31 @@ class ObstacleAvoidance(CartesianPositionController):
                     tf.ConnectivityException,
                     tf.ExtrapolationException):
                 continue
-        # print(time.time() - t)
 
     def get_distance_vectors_end_effector(self):
-        d_vectors_i = []
+        """
+        Get distance vectors that go from obstacle points to the end effector
+
+        Returns
+        -------
+        d_vectors: list
+            List of np.ndarray s, as many as obstacle points there are
+        """
+        d_vectors = []
         number_of_obstacle_points = len(self.obstacle_points)
-
         for j in range(number_of_obstacle_points):
-            d_vectors_i = d_vectors_i + [self.obstacle_points[j] - self.position]
-
-        return d_vectors_i
+            d_vectors = d_vectors + [self.obstacle_points[j] - self.position]
+        return d_vectors
 
     def get_distance_vectors_body(self):
-        # t = time.time()
+        """
+        Get distance vectors that go from obstacle points to control points
+
+        Returns
+        -------
+        D_vectors: list
+            List of lists that contain np.ndarrays, as many as obstacle points there are
+        """
         D_vectors = []
         number_of_control_points = len(self.control_points)
         number_of_obstacle_points = len(self.obstacle_points)
@@ -63,11 +76,17 @@ class ObstacleAvoidance(CartesianPositionController):
             for j in range(number_of_obstacle_points):
                 d_vectors_i = d_vectors_i + [self.obstacle_points[j] - self.control_points[i]]
             D_vectors = D_vectors + [d_vectors_i]
-
-        # print(time.time() - t)
         return D_vectors
 
     def get_repulsive_distance(self):
+        """
+        Repulsive vector algorithm (Only distance information)
+
+        Returns
+        -------
+        max_repulse_vector: np.ndarray
+            Repulsive vector that the algorithm computes having into account obstacle data
+        """
         repulse_vector = []
         distance_vectors = self.get_distance_vectors_end_effector()
 
@@ -76,13 +95,8 @@ class ObstacleAvoidance(CartesianPositionController):
             D_norm = np.linalg.norm(D)
             D_unit_vec = D/D_norm
 
-            # print("D_norm: {}".format(D_norm))
-
             v_mag_repulse = V_MAX * (1 / (1 + np.exp((D_norm * (2 / RHO) - 1) * ALPHA)))
             v_repulse = v_mag_repulse * D_unit_vec
-
-            # print("Repulsive vector norm: {}".format(v_mag_repulse))
-            # print("-------------------------------")
 
             repulse_vector.append(v_repulse)
 
@@ -91,6 +105,15 @@ class ObstacleAvoidance(CartesianPositionController):
         return max_repulse_vector
 
     def get_repulsive_distance_velocity(self):
+        """
+        Repulsive vector algorithm (distance and rate of change information)
+
+        Returns
+        -------
+        max_repulse_vector: np.ndarray
+            Repulsive vector that the algorithm computes having into account obstacle data.
+            The angle of the vector is changed by this algorithm
+        """
 
         self.Vi = self.get_repulsive_distance()
         try:
@@ -112,34 +135,71 @@ class ObstacleAvoidance(CartesianPositionController):
 
         r = self.Vi / np.linalg.norm(self.Vi)
         beta = np.arccos(np.dot(a, r))
-        print(beta)
 
         if beta > np.pi / 2:
-            # The obstacle is moving away from us.
-            # Don't use velocity information
-            return self.Vi
+            return self.Vi  # The obstacle is moving away from us. Don't use velocity information
         else:
             n = np.cross(a, r)
             v = np.cross(n, a)
             new_angle = beta - (np.pi/2 - beta) * np.exp(-(C * (2 / MAX_RATE * Vi_dot_magnitude - 1)))
             V_new = np.linalg.norm(self.Vi) * (np.cos(new_angle) * a + np.sin(new_angle) * v)
-
-            print(new_angle)
-            print('------------------------------------')
-
             return V_new
 
     def search_smallest_vector(self, vectors_list):
+        """
+        Algorithm that takes a list of vectors and returns the smallest
+
+        Parameters
+        ----------
+        vectors_list : list
+            List of np.ndarrays
+
+        Returns
+        -------
+        vectors_list[i]: np.ndarray
+            The smallest vector in vectors_list
+        """
         norms = [np.linalg.norm(v) for v in vectors_list]
         i = norms.index(min(norms))
         return vectors_list[i]
 
     def search_largest_vector(self, vectors_list):
+        """
+        Algorithm that takes a list of vectors and returns the largest
+
+        Parameters
+        ----------
+        vectors_list : list
+            List of np.ndarrays
+
+        Returns
+        -------
+        vectors_list[i]: np.ndarray
+            The largest vector in vectors_list
+        """
         norms = [np.linalg.norm(v) for v in vectors_list]
         i = norms.index(max(norms))
         return vectors_list[i]
 
     def select_most_restrictive(self, q_dot_max_list, q_dot_min_list):
+        """
+        This function gets a list of restriction vectors and returns the most restrictive combination
+
+        Parameters
+        ----------
+        q_dot_max_list : list
+            list of restriction vectors
+        q_dot_min_list : list
+            list of restriction vectors
+
+        Returns
+        -------
+        q_dot_min: np.ndarray
+            restriction vector
+        q_dot_max: np.ndarray
+            restriction vectors
+        """
+
         q_dot_max = np.array(Q_DOT_MAX)
         q_dot_min = np.array(Q_DOT_MIN)
         for i in range(7):
@@ -150,15 +210,40 @@ class ObstacleAvoidance(CartesianPositionController):
         return (q_dot_min, q_dot_max)
 
     def end_effector_algorithm(self, xd_dot):
+        """
+        Algorithm that modifies the end effector velocity with the repulsive vectors, that depend on the obstacles sensed
+
+        Parameters
+        ----------
+        xd_dot : np.ndarray
+            6x1 desired end effector velocity vector
+
+        Returns
+        -------
+        xc_dot : np.ndarray
+            6x1 modified end effector velocity vector (Modified with obstacle information)
+        """
+        # Uncomment one of the following options to decide what algorithm you want to use
         repulsive_vector = np.block([self.get_repulsive_distance(), np.array([0, 0, 0])])
         # repulsive_vector = np.block([self.get_repulsive_distance_velocity(), np.array([0, 0, 0])])
+
         repulsive_vector.shape = (6, 1)
+
         xc_dot = xd_dot - repulsive_vector
 
         return xc_dot
 
     def body_algorithm(self):
-        # t = time.time()
+        """
+        Algorithm that computes the restrictions to joint velocities depending on obstacles data.
+
+        Returns
+        -------
+        q_dot_min: np.ndarray
+            restriction vector
+        q_dot_max: np.ndarray
+            restriction vectors
+        """
         D = self.get_distance_vectors_body()
 
         q_dot_max_list = []
@@ -186,19 +271,21 @@ class ObstacleAvoidance(CartesianPositionController):
             q_dot_max_list = q_dot_max_list + [q_dot_max_i]
             q_dot_min_list = q_dot_min_list + [q_dot_min_i]
 
-            print("distance: {}".format(distance_norm))
-            print("f: {}".format(f))
-
         (q_dot_min, q_dot_max) = self.select_most_restrictive(q_dot_max_list, q_dot_min_list)
-        # print(time.time() - t)
-
-        # print('q_dot_min{}'.format(q_dot_min))
-        # print('q_dot_max{}'.format(q_dot_max))
-        # print('------------------------')
 
         return (q_dot_min, q_dot_max)
 
     def apply_restrictions(self, q_dot_min, q_dot_max):
+        """
+        Applies the restrictions contained in q_dot_min and q_dot_max to self.q_dot member of the controller
+
+        Parameters
+        ----------
+        q_dot_min : [type]
+            [description]
+        q_dot_max : [type]
+            [description]
+        """
         print('Before: {}'.format(np.array(self.q_dot)))
         for i in range(7):
             if self.q_dot[i] > q_dot_max[i]:
@@ -208,21 +295,9 @@ class ObstacleAvoidance(CartesianPositionController):
         print('After: {}'.format(np.array(self.q_dot)))
         print("---------------")
 
-    def is_array_in_list(self, element, list_of_vectors):
-        for vector in list_of_vectors:
-            if np.array_equal(element, vector):
-                return True
-        return False
-
-    def get_obstacle_points(self):
-
-        # self.obstacle_points = [np.array([0.65, 0, 0.3])]
-        self.obstacle_points = [np.array([-0.0, 0.00013597, 0.47066])]
-        return 0
-
     def go_to_point(self, position_desired):
         """
-        Move to a point an stop when arrived
+        Main function. Move to a point an stop when arrived
 
         Parameters
         ----------
@@ -233,13 +308,18 @@ class ObstacleAvoidance(CartesianPositionController):
         position_desired = np.array(position_desired)
         self.error = position_desired - self.position
         while np.linalg.norm(self.error) > self.error_threshold:
+            # Update lists
             self.get_control_points()
             self.get_obstacle_points()
+
+            # Obtained the desired end effector velocity, in the case there were no obstacles
             xd_dot = self.compute_command_velocity(position_desired)
-            # print("xd_dot norm: {}".format(np.linalg.norm(xd_dot)))
+
+            # Modify the velocity with the obstacles information
             # Add flacco algorithm for end effector to get cartesian velocity xc_dot
-            # xc_dot = self.end_effector_algorithm(xd_dot)
-            xc_dot = xd_dot
+            xc_dot = self.end_effector_algorithm(xd_dot)
+            # xc_dot = xd_dot
+
             # Compute the corresponding joint velocities q_dot
             self.q_dot = self.compute_command_q_dot(xc_dot)
             # Compute the joint velocity restrictions and apply them
@@ -251,23 +331,18 @@ class ObstacleAvoidance(CartesianPositionController):
         self.stop()
         return 0
 
+    def get_obstacle_points(self):
+        """
+        In the future this will compute obstacle points.
+        Right now it's just a list of points to test the algorithm.
+        """
+        self.obstacle_points = np.array([[-0.1, 0.00013597, 0.47066]])
+
 
 if __name__ == "__main__":
-    # controller = ObstacleAvoidance()
-    # controller.get_obstacle_points()
-    # controller.get_control_points()
-    # print(controller.get_distance_vectors_end_effector())
-    # print(controller.get_repulsive_distance())
-    # controller.V_i = np.array([0, 0, 0.1])
-    # print(controller.get_repulsive_distance_velocity())
 
-    # distance_norm = np.arange(0.0, 0.5, 0.01)
-    # f = 1 / (1 + np.exp((distance_norm * 2 / RHO - 1) * ALPHA))
-    # plt.plot(distance_norm, f)
-    # plt.show()
-
-    cartesian_controller = ObstacleAvoidance()
-    trajectory = np.array([[0.4, 0, 0.3], [0.55, 0, 0.3]])
+    obstacle_avoidance_controller = ObstacleAvoidanceController()
+    trajectory = np.array([[0.5, 0, 0.3], [0.8, 0, 0.3]])
     while not rospy.is_shutdown():
-        cartesian_controller.go_to_point(trajectory[0])
-        cartesian_controller.go_to_point(trajectory[1])
+        obstacle_avoidance_controller.go_to_point(trajectory[0])
+        obstacle_avoidance_controller.go_to_point(trajectory[1])
