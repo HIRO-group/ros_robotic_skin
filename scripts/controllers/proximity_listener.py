0ffdc59aaf91eb758a9aa0ce33b48f83a21b76de
Ander <ander@pop-os.localdomain>
2020-04-29 17:20:26 -0600

    Reading and visualizing from different proximity sensors and visualizing in rviz

---
+++ scripts/controllers/proximity_listener.py
@@ -0,0 +1,90 @@
+import rospy
+import tf
+from sensor_msgs.msg import LaserScan
+import numpy as np
+from visualization_msgs.msg import Marker
+
+memory = []
+THRESHOLD = 0.03
+
+
+def make_marker(is_add, id, xyz, namespace="live_readings"):
+    msg = Marker()
+    msg.pose.position.x = xyz[0]
+    msg.pose.position.y = xyz[1]
+    msg.pose.position.z = xyz[2]
+    msg.header.frame_id = "world"
+    msg.type = Marker.SPHERE
+    msg.pose.orientation.x = 0.0
+    msg.pose.orientation.y = 0.0
+    msg.pose.orientation.z = 0.0
+    msg.pose.orientation.w = 1.0
+    msg.header.stamp = rospy.Time()
+    msg.ns = namespace
+    if is_add:
+        msg.action = Marker.ADD
+    else:
+        msg.action = Marker.DELETE
+    msg.id = id
+    radius = 0.05
+    msg.scale.x = radius
+    msg.scale.y = radius
+    msg.scale.z = radius
+    msg.color.r = 1.0
+    msg.color.g = 0
+    msg.color.b = 0
+    msg.color.a = 1.0
+    return msg
+
+
+def save_point(vector):
+    global memory
+    if memory:
+        for point in memory:
+            if np.linalg.norm(vector - point) < THRESHOLD:
+                return 0
+    memory.append(vector)
+
+    return 0
+
+
+def callback(data):
+    global trans, rot
+    while True:
+        try:
+            (trans, rot) = tf_listener.lookupTransform('world', 'proximity_link1', rospy.Time(0))
+            break
+        except (tf.LookupException,
+                tf.ConnectivityException,
+                tf.ExtrapolationException):
+            continue
+
+    if data.ranges[0] == float('inf'):
+        # Delete point
+        position_vector = np.array([0, 0, 0])
+        msg = make_marker(False, int(data.header.frame_id[-1]), position_vector)
+        pub.publish(msg)
+
+    else:
+        # Show point
+        translation1 = np.array(trans)
+        translation2 = np.array([data.ranges[0], 0, 0])  # Apply rotation in rot to this vector
+
+        position_vector = translation1 + translation2
+
+        msg = make_marker(True, int(data.header.frame_id[-1]),   position_vector)
+        pub.publish(msg)
+
+        save_point(position_vector)
+
+
+rospy.init_node('proximity_listener')
+tf_listener = tf.TransformListener()
+for i in range(4):
+    rospy.Subscriber("proximity_data{}".format(i), LaserScan, callback)
+
+pub = rospy.Publisher('visualization_marker', Marker, queue_size=1)
+rospy.spin()
+
+# TODO Add the transform feature (krishna quaternions)
+# TODO Memory of objects (have into account if it's  new point and visualize those points, is it in the body spheres)
