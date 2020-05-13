45e014c55a2def28483ed66e3069db30bd88f5c3
Matthew Strong <mast4878@colorado.edu>
2020-05-13 12:37:56 -0600

    working basic test.

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,16 +10,19 @@ import rospy
 ros_robotic_skin_path = rospkg.RosPack().get_path('ros_robotic_skin')
 controllers_path = os.path.join(ros_robotic_skin_path, 'scripts')
 sys.path.insert(0, controllers_path)
-from controllers.RobotController import PandaController
+from controllers.RobotController import PandaController  # noqa: E402
 
 
 class MainTestCase(unittest.TestCase):
     def tester(self):
         rc = PandaController(7)
+        rc.publish_positions([1, 1, 1, 1, 1, 1, 1])
+        rc.publish_positions([0, 0, 0, 0, 0, 0, 0])
+
+        # self.assertAlmostEqual(rc)
         self.assertTrue(rc.is_sim)
 
 
 if __name__ == '__main__':
-    # rospy.wait_for_service('/panda_joint_trajectory_controller/query_state')
-    rospy.sleep(40)
+    rospy.sleep(30)
     rostest.rosrun('ros_robotic_skin', 'test_main', MainTestCase)
