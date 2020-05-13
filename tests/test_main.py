4696d7d612d40be5f4c88898314907cbab36ae01
Matthew Strong <mast4878@colorado.edu>
2020-05-13 11:05:47 -0600

    example

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,15 +10,16 @@ import rospy
 ros_robotic_skin_path = rospkg.RosPack().get_path('ros_robotic_skin')
 controllers_path = os.path.join(ros_robotic_skin_path, 'scripts')
 sys.path.insert(0, controllers_path)
-from controllers.RobotController import RobotController
-os.system("pwd")
+from controllers.RobotController import PandaController
 
 
 class MainTestCase(unittest.TestCase):
     def tester(self):
-        rc = RobotController(7)
-        pass
+        rc = PandaController(7)
+        self.assertTrue(rc.is_sim)
 
 
 if __name__ == '__main__':
+    # rospy.wait_for_service('/panda_joint_trajectory_controller/query_state')
+    rospy.sleep(40)
     rostest.rosrun('ros_robotic_skin', 'test_main', MainTestCase)
