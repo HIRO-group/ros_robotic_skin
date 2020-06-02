c612f1b943149cd194b3ad2ad2228673d01bd0d0
watakandai <kandai.wata@gmail.com>
2020-06-02 03:19:49 +0000

    Deploying to gh-pages from  @ f75acb9b01a3e3282000c8c9f4e49754d5879484 🚀

--- menudata.js
+++ menudata.js
@@ -119,6 +119,7 @@ var menudata={children:[
 {text:"s",url:"functions_vars_s.html#index_s"},
 {text:"t",url:"functions_vars_t.html#index_t"},
 {text:"v",url:"functions_vars_v.html#index_v"},
+{text:"w",url:"functions_vars_w.html#index_w"},
 {text:"x",url:"functions_vars_x.html#index_x"}]}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
