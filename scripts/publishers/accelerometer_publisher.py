fb56104e85ffec46dfaad89e7d5d8fbbd5b72731
Krishna <chaitanyaradon89@gmail.com>
2020-06-07 15:09:03 -0600

    Remove rospy rate sleep and replace it with traditional python sleep

--- scripts/publishers/accelerometer_publisher.py
+++ scripts/publishers/accelerometer_publisher.py
@@ -1,5 +1,6 @@
 from robotic_skin.sensor.lsm6ds3_accel import LSM6DS3_acclerometer
 from robotic_skin.sensor.lsm6ds3_gyro import LSM6DS3_gyroscope
+from time import sleep
 from sensor_msgs.msg import Imu
 import rospy
 import rospkg
@@ -17,7 +18,6 @@ if __name__ == "__main__":
     gyro = LSM6DS3_gyroscope(config_file)
     rospy.init_node('talker_%s' % str(accel.config_dict['imu_number']), anonymous=True)
     pub = rospy.Publisher('/imu_data%s' % str(accel.config_dict['imu_number']), Imu, queue_size=10)
-    r = rospy.Rate(100)
     imu_msg = Imu()
     while not rospy.is_shutdown():
         accel_list = accel.read()
@@ -30,4 +30,4 @@ if __name__ == "__main__":
         imu_msg.angular_velocity.y = gyro_list[1]
         imu_msg.angular_velocity.z = gyro_list[2]
         pub.publish(imu_msg)
-        r.sleep()
+        sleep(0.1)
