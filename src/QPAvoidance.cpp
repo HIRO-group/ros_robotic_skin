b365a8133717fc9d15019fb71e8d1fa36a7b4d0e
aran159 <ander.aranfdz@gmail.com>
2020-06-15 16:10:55 -0600

    Debugging: Equation 5 is working

--- src/QPAvoidance.cpp
+++ src/QPAvoidance.cpp
@@ -2,93 +2,101 @@
 
 Eigen::VectorXd QPAvoidance::algLib(Eigen::MatrixXd H, Eigen::VectorXd f, Eigen::MatrixXd A, Eigen::VectorXd b, Eigen::VectorXd bl, Eigen::VectorXd bu)
 {
-    ALGLIBAb = "[[]]";
-    ALGLIBconstraintType = "[]";
-
-    for (int i = 0; i < H.rows(); i++)
-    {
-        for (int j = 0; j < H.cols(); j++)
-        {
-            ALGLIBH(i,j) = H(i,j);
-
-        }
-        ALGLIBf(i) = f(i);
-        ALGLIBbl(i) = bl(i);
-        ALGLIBbu(i) = bu(i);
-    }
-
-    if (A.rows() == 0)
+    try
     {
         ALGLIBAb = "[[]]";
-    }
-    else
-    {
-        ALGLIBAb.setlength(A.rows(), A.cols() + 1);
-        ALGLIBconstraintType.setlength(A.rows());
-        for (int i = 0; i < A.rows(); i++)
+        ALGLIBconstraintType = "[]";
+
+        for (int i = 0; i < H.rows(); i++)
         {
-            for (int j = 0; j < A.cols(); j++)
+            for (int j = 0; j < H.cols(); j++)
             {
-                ALGLIBAb(i,j) = A(i,j);
+                ALGLIBH(i,j) = H(i,j);
+
             }
-            ALGLIBAb(i, A.cols()) = b(i);
-            ALGLIBconstraintType(i) = -1;
+            ALGLIBf(i) = f(i);
+            ALGLIBbl(i) = bl(i);
+            ALGLIBbu(i) = bu(i);
         }
+
+        if (A.rows() == 0)
+        {
+            ALGLIBAb = "[[]]";
+        }
+        else
+        {
+            ALGLIBAb.setlength(A.rows(), A.cols() + 1);
+            ALGLIBconstraintType.setlength(A.rows());
+            for (int i = 0; i < A.rows(); i++)
+            {
+                for (int j = 0; j < A.cols(); j++)
+                {
+                    ALGLIBAb(i,j) = A(i,j);
+                }
+                ALGLIBAb(i, A.cols()) = b(i);
+                ALGLIBconstraintType(i) = -1;
+            }
+        }
+
+        // create solver, set quadratic/linear terms
+        alglib::minqpcreate(ALGLIBH.cols(), ALGLIBstate);
+        alglib::minqpsetquadraticterm(ALGLIBstate, ALGLIBH);
+        alglib::minqpsetlinearterm(ALGLIBstate, ALGLIBf);
+        alglib::minqpsetlc(ALGLIBstate, ALGLIBAb, ALGLIBconstraintType);
+        alglib::minqpsetbc(ALGLIBstate, ALGLIBbl, ALGLIBbu);
+
+        // Set scale of the parameters.
+        // It is strongly recommended that you set scale of your variables.
+        // Knowing their scales is essential for evaluation of stopping criteria
+        // and for preconditioning of the algorithm steps.
+        // You can find more information on scaling at http://www.alglib.net/optimization/scaling.php
+        //
+        // NOTE: for convex problems you may try using minqpsetscaleautodiag()
+        //       which automatically determines variable scales.
+        alglib::minqpsetscale(ALGLIBstate, ALGLIBscale);
+
+
+        // SOLVE: 3 options: BLEIC-based, DENSE-AUL, QUICKQP
+
+        // BLEIC-based QP solver is intended for problems with moderate (up to 50) number
+        // of general linear constraints and unlimited number of box constraints.
+        //
+        // Default stopping criteria are used.
+        //
+        alglib::minqpsetalgobleic(ALGLIBstate, 0.0, 0.0, 0.0, 0);
+        alglib::minqpoptimize(ALGLIBstate);
+        alglib::minqpresults(ALGLIBstate, ALGLIBqDot, ALGLIBrep);
+        // printf("%s\n", ALGLIBqDot.tostring(1).c_str()); // EXPECTED: [1.500,0.500]
+
+
+        // DENSE-AUL solver is optimized for problems with up to several thousands of
+        // variables and large amount of general linear constraints. Problems with
+        // less than 50 general linear constraints can be efficiently solved with
+        // BLEIC, problems with box-only constraints can be solved with QuickQP.
+        // However, DENSE-AUL will work in any (including unconstrained) case.
+        //
+        // Default stopping criteria are used.
+        //
+        // alglib::minqpsetalgodenseaul(ALGLIBstate, 1.0e-9, 1.0e+4, 5);
+        // alglib::minqpoptimize(ALGLIBstate);
+        // alglib::minqpresults(ALGLIBstate, ALGLIBqDot, ALGLIBrep);
+        // printf("%s\n", ALGLIBqDot.tostring(1).c_str()); // EXPECTED: [1.500,0.500]
+
+        // QuickQP solver is intended for medium and large-scale problems with box
+        // constraints, and...
+        //
+        // ...Oops! It does not support general linear constraints, -5 returned as completion code!
+        //
+        // alglib::minqpsetalgoquickqp(ALGLIBstate, 0.0, 0.0, 0.0, 0, true);
+        // alglib::minqpoptimize(ALGLIBstate);
+        // alglib::minqpresults(ALGLIBstate, ALGLIBqDot, ALGLIBrep);
+        // printf("%d\n", int(ALGLIBrep.terminationtype)); // EXPECTED: -5 (In our case positive)
+    }
+    catch(alglib::ap_error e)
+    {
+        printf("error msg: %s\n", e.msg.c_str());
     }
 
-    // create solver, set quadratic/linear terms
-    alglib::minqpcreate(ALGLIBH.cols(), ALGLIBstate);
-    alglib::minqpsetquadraticterm(ALGLIBstate, ALGLIBH);
-    alglib::minqpsetlinearterm(ALGLIBstate, ALGLIBf);
-    alglib::minqpsetlc(ALGLIBstate, ALGLIBAb, ALGLIBconstraintType);
-    alglib::minqpsetbc(ALGLIBstate, ALGLIBbl, ALGLIBbu);
-
-    // Set scale of the parameters.
-    // It is strongly recommended that you set scale of your variables.
-    // Knowing their scales is essential for evaluation of stopping criteria
-    // and for preconditioning of the algorithm steps.
-    // You can find more information on scaling at http://www.alglib.net/optimization/scaling.php
-    //
-    // NOTE: for convex problems you may try using minqpsetscaleautodiag()
-    //       which automatically determines variable scales.
-    alglib::minqpsetscale(ALGLIBstate, ALGLIBscale);
-
-
-    // SOLVE: 3 options: BLEIC-based, DENSE-AUL, QUICKQP
-
-    // BLEIC-based QP solver is intended for problems with moderate (up to 50) number
-    // of general linear constraints and unlimited number of box constraints.
-    //
-    // Default stopping criteria are used.
-    //
-    alglib::minqpsetalgobleic(ALGLIBstate, 0.0, 0.0, 0.0, 0);
-    alglib::minqpoptimize(ALGLIBstate);
-    alglib::minqpresults(ALGLIBstate, ALGLIBqDot, ALGLIBrep);
-    // printf("%s\n", ALGLIBqDot.tostring(1).c_str()); // EXPECTED: [1.500,0.500]
-
-
-    // DENSE-AUL solver is optimized for problems with up to several thousands of
-    // variables and large amount of general linear constraints. Problems with
-    // less than 50 general linear constraints can be efficiently solved with
-    // BLEIC, problems with box-only constraints can be solved with QuickQP.
-    // However, DENSE-AUL will work in any (including unconstrained) case.
-    //
-    // Default stopping criteria are used.
-    //
-    // alglib::minqpsetalgodenseaul(ALGLIBstate, 1.0e-9, 1.0e+4, 5);
-    // alglib::minqpoptimize(ALGLIBstate);
-    // alglib::minqpresults(ALGLIBstate, ALGLIBqDot, ALGLIBrep);
-    // printf("%s\n", ALGLIBqDot.tostring(1).c_str()); // EXPECTED: [1.500,0.500]
-
-    // QuickQP solver is intended for medium and large-scale problems with box
-    // constraints, and...
-    //
-    // ...Oops! It does not support general linear constraints, -5 returned as completion code!
-    //
-    // alglib::minqpsetalgoquickqp(ALGLIBstate, 0.0, 0.0, 0.0, 0, true);
-    // alglib::minqpoptimize(ALGLIBstate);
-    // alglib::minqpresults(ALGLIBstate, ALGLIBqDot, ALGLIBrep);
-    // printf("%d\n", int(ALGLIBrep.terminationtype)); // EXPECTED: -5 (In our case positive)
 
     for (int i = 0; i < ALGLIBqDot.length(); i++)
     {
@@ -162,67 +170,69 @@ Eigen::VectorXd QPAvoidance::computeJointVelocities(Eigen::VectorXd q, Eigen::Ve
     Eigen::MatrixXd H = J.transpose() * J + computeDampingFactor(std::sqrt((J*J.transpose()).determinant())) * Eigen::MatrixXd::Identity(7,7);
     Eigen::VectorXd f = - xDot.transpose() * J;
 
-    // Being m the number of obstacle points detected:
-    // See equation #5 in ding paper for first m rows, then an additonal 1 row for equation #7
-    Eigen::MatrixXd A = Eigen::MatrixXd::Zero(obstaclePositionVectors.size() + 1, 7);
-    // b is from equation #8, must have the same number of rows as A
-    Eigen::VectorXd b(obstaclePositionVectors.size() + 1);
-    // Will set w dependent on the distance value
-    Eigen::VectorXd w = Eigen::VectorXd::Ones(obstaclePositionVectors.size());
-    // C comes from equation #6, intermediate value
-    Eigen::MatrixXd C(obstaclePositionVectors.size(), 7), Jpc, JpcNormalized = Eigen::MatrixXd::Zero(3, 7);
-    // temp variable used to hold distance
-    Eigen::Vector3d d;
-    // temp variable used to pick the control point closest to each of obstacle points
-    std::vector<double> distancesToControlPoints, distanceNorms;
-    distancesToControlPoints.resize(numberControlPoints);
-    distanceNorms.resize(obstaclePositionVectors.size());
-
-    int assignedIndex{0};
-    double distanceNormsSum{0};
-
-    // 1) select the control point closes to each obstacle
-    // 2) Calculate the distance d from the obstacle point to chosen control point
-    // 3) Get Jacobian to selected control point. This is changing is size dependent on what control point we have selected
-    // 4) Alter the size of the Jacobian so that it's always 7x3
-    // 5) Add extra row from equation 5
-    // 6) d norm calculation done in Fig. 5
-    // 7) Take the gradient os the norm distance with
-    for (int i = 0; i < obstaclePositionVectors.size(); i++)
-    {
-        for (int j = 0; j < numberControlPoints; j++)
-            distancesToControlPoints[j] = (obstaclePositionVectors[i] - controlPointPositionVectors[j]).norm();
-        // assignedIndex = std::distance(distancesToControlPoints.begin(), std::min_element(distancesToControlPoints.begin(), distancesToControlPoints.end()));
-        assignedIndex = 3;
-        d = obstaclePositionVectors[i] - kdlSolver.forwardKinematics(std::string("control_point") + std::to_string(assignedIndex), q);
-        distanceNorms[i] = d.norm();
-        distanceNormsSum = distanceNormsSum + distanceNorms[i];
-        Jpc = kdlSolver.computeJacobian(std::string("control_point") + std::to_string(assignedIndex), q);
-        JpcNormalized.block(0, 0, 3, Jpc.cols()) = Jpc.block(0, 0, 3, Jpc.cols());
-        A.row(i) = d.normalized().transpose() * JpcNormalized;
-        b[i] = computebvalue(distanceNorms[i]); // From fig. 5
-        C.row(i) = gradientOfDistanceNorm(obstaclePositionVectors[i], std::string("control_point") + std::to_string(assignedIndex), q);
-
-        ///////
-        // std::cout << assignedIndex << std::endl;
-        // std::cout << Jpc.rows() << "," << Jpc.cols() << std::endl;
-        ////////
-    }
-
-    // Calculate the weights to complet equation #7
-    for (int i = 0; i < obstaclePositionVectors.size(); i++)
-    {
-        w[i] = distanceNorms[i] / distanceNormsSum;
-    }
-    // Last row in A, equation #7
-    A.row(obstaclePositionVectors.size()) = - w.transpose() * C;
-    b[obstaclePositionVectors.size()] = 0;
-
+    Eigen::MatrixXd A;
+    Eigen::VectorXd b;
     if (obstaclePositionVectors.size() == 0)
     {
-        A = Eigen::MatrixXd(0,7);
+        A = Eigen::MatrixXd(0,0);
         b = Eigen::VectorXd(0);
     }
+    else
+    {
+        // Being m the number of obstacle points detected:
+        // See equation #5 in ding paper for first m rows, then an additonal 1 row for equation #7
+        A = Eigen::MatrixXd(obstaclePositionVectors.size() + 1, 7);
+        // b is from equation #8, must have the same number of rows as A
+        b = Eigen::VectorXd(obstaclePositionVectors.size() + 1);
+        // Will set w dependent on the distance value
+        Eigen::VectorXd w = Eigen::VectorXd::Ones(obstaclePositionVectors.size());
+        // C comes from equation #6, intermediate value
+        Eigen::MatrixXd C(obstaclePositionVectors.size(), 7), Jpc, JpcNormalized = Eigen::MatrixXd::Zero(3, 7);
+        // temp variable used to hold distance
+        Eigen::Vector3d d;
+        // temp variable used to pick the control point closest to each of obstacle points
+        std::vector<double> distancesToControlPoints, distanceNorms;
+        distancesToControlPoints.resize(numberControlPoints);
+        distanceNorms.resize(obstaclePositionVectors.size());
+
+        int assignedIndex{0};
+        double distanceNormsSum{0};
+
+        // 1) select the control point closes to each obstacle
+        // 2) Calculate the distance d from the obstacle point to chosen control point
+        // 3) Get Jacobian to selected control point. This is changing is size dependent on what control point we have selected
+        // 4) Alter the size of the Jacobian so that it's always 7x3
+        // 5) Add extra row from equation 5
+        // 6) d norm calculation done in Fig. 5
+        // 7) Take the gradient os the norm distance with
+        for (int i = 0; i < obstaclePositionVectors.size(); i++)
+        {
+            for (int j = 0; j < numberControlPoints; j++)
+                distancesToControlPoints[j] = (obstaclePositionVectors[i] - controlPointPositionVectors[j]).norm();
+            assignedIndex = std::distance(distancesToControlPoints.begin(), std::min_element(distancesToControlPoints.begin(), distancesToControlPoints.end()));
+            d = obstaclePositionVectors[i] - kdlSolver.forwardKinematics(std::string("control_point") + std::to_string(assignedIndex), q);
+            distanceNorms[i] = d.norm();
+            distanceNormsSum = distanceNormsSum + distanceNorms[i];
+            Jpc = kdlSolver.computeJacobian(std::string("control_point") + std::to_string(assignedIndex), q);
+            JpcNormalized.block(0, 0, 3, Jpc.cols()) = Jpc.block(0, 0, 3, Jpc.cols());
+            A.row(i) = d.normalized().transpose() * JpcNormalized;
+            b[i] = computebvalue(distanceNorms[i]); // From fig. 5
+            C.row(i) = gradientOfDistanceNorm(obstaclePositionVectors[i], std::string("control_point") + std::to_string(assignedIndex), q);
+        }
+
+        // Calculate the weights to complet equation #7
+        for (int i = 0; i < obstaclePositionVectors.size(); i++)
+        {
+            w[i] = distanceNorms[i] / distanceNormsSum;
+        }
+
+        // Last row in A, equation #7
+        A.row(obstaclePositionVectors.size()) = - w.transpose() * C;
+        b[obstaclePositionVectors.size()] = 0;
+        A.conservativeResize(obstaclePositionVectors.size(), 7);
+        b.conservativeResize(obstaclePositionVectors.size());
+    }
+
 
 
     //////
