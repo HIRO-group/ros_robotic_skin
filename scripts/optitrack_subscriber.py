0f213e8828b5b4cdb2f5424defded29636c20e3d
calebescobedo <caleb.escobedo@colorado.edu>
2020-07-31 17:35:13 -0600

    flake8 changes

--- scripts/optitrack_subscriber.py
+++ scripts/optitrack_subscriber.py
@@ -4,21 +4,20 @@ import rospy
 import rospkg
 from geometry_msgs.msg import PoseStamped
 
+
 def save(pickle_data):
-    
     ros_robotic_skin_path = rospkg.RosPack().get_path('ros_robotic_skin')
     filepath = os.path.join(ros_robotic_skin_path, 'data/optitrack_data.pickle')
 
     with open(filepath, 'ab') as f:
         pickle.dump(pickle_data, f)
 
+
 def callback(data):
-    #rospy.loginfo(rospy.get_caller_id() + 'I heard %s ', data)
+    # rospy.loginfo(rospy.get_caller_id() + 'I heard %s ', data)
     save(data)
 
 def listener():
-
-
     rospy.init_node('listener', anonymous=True)
 
     topic_name = "/vrpn_client_node/RigidBody01/pose"
@@ -28,19 +27,12 @@ def listener():
     # spin() simply keeps python from exiting until this node is stopped
     rospy.sleep(3)
 
+
 if __name__ == '__main__':
     ros_robotic_skin_path = rospkg.RosPack().get_path('ros_robotic_skin')
     filepath = os.path.join(ros_robotic_skin_path, 'data/optitrack_data.pickle')
+
     if os.path.exists(filepath):
         os.remove(filepath)
-    
     listener()
-   
-    
-    # with open(filepath, 'rb') as f:
-    #     while True:
-    #         try:
-    #             print(pickle.load(f))
-    #         except EOFError:
-    #             break
 
