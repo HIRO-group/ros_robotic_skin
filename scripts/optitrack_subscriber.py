694886e34a6bd064a93d614a1ac4cb1ba9e7797a
dhruvpatel.aj@gmail.com <dhruvpatel.aj@gmail.com>
2020-08-02 16:21:28 -0600

    only writes once to pickle file, stores all data into a list beforehand

--- scripts/optitrack_subscriber.py
+++ scripts/optitrack_subscriber.py
@@ -9,21 +9,35 @@ ROS_PATH = rospkg.RosPack().get_path('ros_robotic_skin')
 FILEPATH = os.path.join(ROS_PATH, 'data/optitrack_data.pickle')
 
 
-def save_to_pickle(pickle_data):
+class poseData:
     """
-    Saves the data to a pickle file.
-    Arguments
-    ----------
-    `data`: `PoseStamped`
-        The data to be saved
+    Class for collecting all pose data for the objects in optitrack
     """
+    def __init__(self):
+        """
+        Initializes the list that stores all data
+        """
+        self.pose_data = []
 
-    with open(FILEPATH, 'ab') as f:
-        pickle.dump(pickle_data, f)
-        # print(pickle_data)
+    def save_to_pickle(self, pickle_data):
+        """
+        Adds all optitrack data to a list
+        Arguments
+        ----------
+        `picke_data`: `PoseStamped`
+            The data to be added to the list
+        """
+        self.pose_data.append(pickle_data)
+
+    def writePickleFile(self):
+        """
+        Saves the data from the list to a pickle file.
+        """
+        with open(FILEPATH, 'wb') as f:
+            pickle.dump(self.pose_data, f)
 
 
-def optitrack_listener(obj):
+def optitrack_listener(topic, poseDataClass):
     """
     Subscribes to events from the optitrack and get PoseStamped data for the
     location of the given object and sends callback data to `save_to_pickle`
@@ -33,9 +47,9 @@ def optitrack_listener(obj):
         The object the optitrack listener will track
     """
 
-    # initializes the subscriber and listens to the object provided through the command line and sends callback data
+    # initializes the subscriber
     rospy.init_node('optitrack_listener', anonymous=True)
-    rospy.Subscriber("/vrpn_client_node/"+obj+"/pose", PoseStamped, save_to_pickle)
+    rospy.Subscriber(topic[0], PoseStamped, poseDataClass.save_to_pickle)
 
     # saves 3 seconds of data
     rospy.sleep(3)
@@ -45,13 +59,16 @@ if __name__ == '__main__':
 
     # accesses command line arguments through rospy
     args = rospy.myargv(argv=sys.argv)
+
     if len(args) != 2:
         raise ValueError("RigidBody not provided!")
-    elif not args[1] in rospy.get_published_topics():
+
+    topic = ["/vrpn_client_node/"+args[1]+"/pose", 'geometry_msgs/PoseStamped']
+    if not topic in rospy.get_published_topics():
         raise ValueError("RigidBody does not exist!")
 
-    # deletes the file before starting to get a clean dataset.
-    if os.path.exists(FILEPATH):
-        os.remove(FILEPATH)
+    newSave = poseData()
+    optitrack_listener(topic, newSave)
 
-    optitrack_listener(args[1])
+    # calls the write function to save the data
+    newSave.writePickleFile()
