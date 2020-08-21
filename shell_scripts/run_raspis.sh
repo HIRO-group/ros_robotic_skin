fc80234e63afa3871ed24ebb07a15816b193c921
BJ Kim <byki1141@colorado.edu>
2020-08-20 23:08:06 -0600

    Fixing robot_ip back to HIRO lab

--- shell_scripts/run_raspis.sh
+++ shell_scripts/run_raspis.sh
@@ -1,4 +1,4 @@
-gnome-terminal -e 'roscore'
+gnome-terminal -e 'roscore' # This line can be removed if roscore is already running
 gnome-terminal -e "ssh -t rp14 'source ~/.bashrc; source /opt/ros/melodic/setup.bash; export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/hiro/catkin_ws; sleep 5; cd catkin_ws/src/ros_robotic_skin/scripts/publishers/; python accelerometer_publisher.py; exec bash'"
 gnome-terminal -e "ssh -t rp14 'source ~/.bashrc; source /opt/ros/melodic/setup.bash; export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/hiro/catkin_ws; sleep 5; cd catkin_ws/src/ros_robotic_skin/scripts/publishers/; python accelerometer_publisher.py; exec bash'"
 gnome-terminal -e "ssh -t rp23 'source ~/.bashrc; source /opt/ros/melodic/setup.bash; export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/hiro/catkin_ws; sleep 5; cd catkin_ws/src/ros_robotic_skin/scripts/publishers/; python accelerometer_publisher.py; exec bash'"
