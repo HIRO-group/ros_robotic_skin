d2a944c0f12852e999273d70ab293e88bf85feb2
BJ Kim <byki1141@colorado.edu>
2020-08-24 00:30:48 -0600

    adding dependency

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@
 
 # TODO: Need to check if we are currently in catkin_ws/src
 cd .. && git clone git@github.com:HIRO-group/hiro_ros_arm_controller.git
+git clone https://github.com/HIRO-group/imu_calib
 cd hiro_ros_arm_controller && ./install.sh
 cd ../.. && catkin build
